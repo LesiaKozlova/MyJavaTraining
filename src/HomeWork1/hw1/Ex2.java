1334e6a720a0826116eb3c5e9f6c1012d0eced27
Lesia Kozlova <lesia.kozlova1981@gmail.com>
2023-03-10 20:18:12 +0100

    test.

---
+++ src/HomeWork1/hw1/Ex2.java
@@ -0,0 +1,13 @@
+package HomeWork1.hw1;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        double chislitel = 7.0;
+        double znamenatel = 5.0;
+        double chastnoe = chislitel/znamenatel;
+        System.out.println("Целая часть равна - "+(int) chastnoe);
+        System.out.println("Остаток от деления равен - "+(int) (chislitel%znamenatel));
+        System.out.println("Знаменатель равен - "+(int) znamenatel);
+
+    }
+}
