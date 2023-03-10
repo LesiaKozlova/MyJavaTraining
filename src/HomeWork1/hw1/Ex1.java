b0c197b422f2ad71bcf1ce5043053e96ad5663c0
Lesia Kozlova <lesia.kozlova1981@gmail.com>
2023-03-10 20:04:54 +0100

    test.

---
+++ src/HomeWork1/hw1/Ex1.java
@@ -0,0 +1,14 @@
+package HomeWork1.hw1;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+        a = 16 - 7;
+        b = 16 - 9;
+//        System.out.println(String.valueOf(a));
+//        System.out.println(String.valueOf(b));
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
